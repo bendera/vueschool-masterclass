4b6f85b7512b417dbbc6a13b1b9e30498ffd6f5e
abender <bender.adam@netpositive.hu>
2018-07-11 17:16:34 +0200

    Introducing the vue-router

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,40 @@
 <template>
   <div>
-    <h1>Welcome to the Forum</h1>
+    <div
+      v-for="thread in threads"
+      :key="thread.id"
+      class="col-large push-top"
+    >
+      <h1>{{ thread.title }}</h1>
+      <div class="post-list">
+        <!-- eslint-disable-next-line vue/require-v-for-key -->
+        <div v-for="postId in thread.posts" class="post">
+          <div class="user-info">
+            <a href="profile.html#profile-details" class="user-name">{{ users[posts[postId].userId].name }}</a>
+            <a href="profile.html#profile-details">
+              <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="">
+            </a>
+            <p class="desktop-only text-small">107 posts</p>
+          </div>
+          <div class="post-content">
+            <div>
+              {{ posts[postId].text }}
+            </div>
+          </div>
+          <div class="post-date text-faded">
+            {{ posts[postId].publishedAt }}
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <h1>Welcome to the Forum</h1>
     <div v-for="thread in threads" :key="thread.id">
       <h2>{{ thread.title }}</h2>
       <div v-for="postId in thread.posts" :key="postId">
         <p>{{ users[posts[postId].userId].name }}</p>
         <p>{{ posts[postId].text }}</p>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -28,7 +55,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
