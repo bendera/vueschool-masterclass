b44c46a714fa85dfb81301563f771e351b9d80e7
abender <bender.adam@netpositive.hu>
2018-07-12 16:53:39 +0200

    handling not found pages

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="col-full">
+  <h1>Not found</h1>
+  <p>404 not found</p>
+  <router-link :to="{name: 'Home'}">
+    Home
+  </router-link>
+</div>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 100px;
+}
+
+p {
+  font-size: 50px;
+  font-weight: 100;
+}
+
+div {
+  text-align: center;
+}
+</style>
+
