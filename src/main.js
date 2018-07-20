3777bae2cbd88090c62db3846e3b0cd3ee0d8782
abender <bender.adam@netpositive.hu>
2018-07-20 12:54:09 +0200

    Global Base Components

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import AppDate from '@/components/AppDate';
 import App from './App';
 import router from './router';
+
+Vue.component('AppDate', AppDate);
 
 Vue.config.productionTip = false;
 
