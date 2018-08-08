38dea3a665f2380fd82f0dc50a4ce098f2a845c1
abender <bender.adam@netpositive.hu>
2018-08-08 17:36:59 +0200

    Small Improvements Before the Firebase Chapter

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -5,6 +5,7 @@
     <a href="profile.html#profile-details">
       <img class="avatar-large" :src="user.avatar" alt="">
     </a>
+    <p class="desktop-only text-small">{{ userThreadsCount }} threads</p>
     <p class="desktop-only text-small">{{ userPostsCount }} posts</p>
   </div>
   <div class="post-content">
@@ -51,6 +52,9 @@ export default {
     },
     userPostsCount() {
       return this.$store.getters.userPostsCount(this.post.userId);
+    },
+    userThreadsCount() {
+      return this.$store.getters.userThreadsCount(this.post.userId);
     },
   },
 };
