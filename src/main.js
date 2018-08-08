80817d3e4d2f1d969099f92397d36c97a3576374
abender <bender.adam@netpositive.hu>
2018-08-08 17:52:27 +0200

    Install Firebase Realtime Database

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import firebase from 'firebase/app';
 import AppDate from '@/components/AppDate';
 import App from './App';
 import store from './store';
@@ -9,6 +10,16 @@ import router from './router';
 Vue.component('AppDate', AppDate);
 
 Vue.config.productionTip = false;
+
+const config = {
+  apiKey: 'AIzaSyAIFGRM-ek-IIct5eUycEboODTK0UB9YFM',
+  authDomain: 'vue-school-forum-6be3a.firebaseapp.com',
+  databaseURL: 'https://vue-school-forum-6be3a.firebaseio.com',
+  projectId: 'vue-school-forum-6be3a',
+  storageBucket: '',
+  messagingSenderId: '583149945610',
+};
+firebase.initializeApp(config);
 
 /* eslint-disable no-new */
 new Vue({
